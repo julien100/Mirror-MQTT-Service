fffb82b5d742195c9c769797270fa00e6d7f6d32
Julien Rensch <julienrensch@Juliens-MBP.fritz.box>
2017-01-01 22:30:09 +0100

    lala

--- MMM-MQTT-Service.js
+++ MMM-MQTT-Service.js
@@ -41,6 +41,7 @@ Module.register('MMM-MQTT-Service',{
 					} else if (payload.value === 'comic') {
 						moduleToHide = 'DailyXKCD';
 					}
+
 					MM.getModules().withClass(moduleToHide).enumerate(function(module) {
 	    				module.hide(1000, function() {
                     	//Module hidden.
@@ -56,10 +57,22 @@ Module.register('MMM-MQTT-Service',{
 					} else if (payload.value === 'comic') {
 						moduleToShow = 'DailyXKCD';
 					}
+					var position;
 					MM.getModules().withClass(moduleToShow).enumerate(function(module) {
-	    				module.show(1000, function() {
-                    	//Module shown.
-                		});
+						position = module.config.position;
+					});
+
+					MM.getModules().enumerate(function(module) {
+						if (module.config.name === moduleToShow) {
+							module.show(1000, function() {
+											//Module shown.
+										});
+						} else if (module.config.position === position) {
+							// verstecke das Modul, das aktuell an der Stelle des zu zeigenden Moduls ist
+							module.hide(1000, function() {
+											//Module hidden.
+										});
+						}
 					});
 				}
 			} else if (payload.receiver === "PODCAST") {
